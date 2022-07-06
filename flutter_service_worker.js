004ad15c15e44a7da3bfb4fe75460a22e7cc9b05
hamididv <100961324+hamididv@users.noreply.github.com>
2022-07-06 17:46:26 +0100

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -39,9 +39,9 @@ const RESOURCES = {
 "flutter.js": "eb2682e33f25cd8f1fc59011497c35f8",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "d5d7aedeb69762a1e8dd455a20f3fdf2",
-"/": "d5d7aedeb69762a1e8dd455a20f3fdf2",
-"main.dart.js": "6d6abdb655e8ed70370507417077c9c3",
+"index.html": "7cf73e204cf8291c4e7b92264380a0b0",
+"/": "7cf73e204cf8291c4e7b92264380a0b0",
+"main.dart.js": "8260857b3c7f74aa9a92b1919ff43e4d",
 "manifest.json": "cbd298b6f14d6da186cf20a738aa8bd7",
 "version.json": "cd0368d7cb50eb830a4e66b31d306c9f"
 };
